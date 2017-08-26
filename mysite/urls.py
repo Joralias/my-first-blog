7957e51cee8b529b31b00fd12085796e1048eafc
Jorge <j.alimarco@gmail.com>
2017-08-26 13:40:36 +0200

    Add loging functionality

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 
+from django.contrib.auth import views
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-
+    url(r'^accounts/login/$', views.login, name='login')
 ]
