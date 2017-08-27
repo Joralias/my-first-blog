8578b69970f836b0e54b3afde9315edba384e116
Jorge <j.alimarco@gmail.com>
2017-08-27 15:48:44 +0200

    Separate app from site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib.auth import views as django_views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -13,5 +14,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
     url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
     url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
-
+    url(r'^accounts/login/$', django_views.login, name='login'),
+    url(r'^accounts/logout/$', django_views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
